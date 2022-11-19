e7bc430a07c2cc8a56864b62dcf02c84a1f55f70
leemolton <leemolton@hotmail.co.uk>
2022-11-19 11:43:51 +0000

    Update wsgi.py

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+  if path not in sys.path:
+    sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
 application = get_wsgi_application()
